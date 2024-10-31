2c4023e3472fd877444637e9ee9b7d9a636063a5
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-31 14:08:00 -0300

    feat: global styles add

--- src/main/config/app.tsx
+++ src/main/config/app.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import HomePage from '@/presentation/pages/home'
+import '@/presentation/styles/global/global-styles.scss'
 
 export default function App (): JSX.Element {
   return <HomePage />
