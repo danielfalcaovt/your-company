b7069dcdc2c586a4600349218beec0fd5a2edd9b
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-31 15:59:42 -0300

    feat: warning decreasing element add

---
+++ src/presentation/components/main/about-us/elements/warning/warning-decreasing.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import * as styles from './warning-decreasing-styles.scss'
+
+interface WarningValues {
+  value: string
+}
+
+export default function WarningDecreasing (props: WarningValues): JSX.Element {
+  return (
+    <div className={styles.decreasing}>
+      <img src='/imgs/assets/decrease.png' alt='decreasing chart'></img>
+      <span>{props.value}</span>
+    </div>
+  )
+}
