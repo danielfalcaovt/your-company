8b91bfc9a2fb413ea80a939cef692311ad48e90d
Daniel <danielfalcaoviktorio@gmail.com>
2024-11-01 11:29:53 -0300

    feat: increasing and decreasing warning element add

---
+++ src/presentation/components/main/about-us/elements/warning/warning-increasing.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import * as styles from './warning-increasing-styles.scss'
+
+interface WarningValues {
+  value: string
+}
+
+export default function WarningIncreasing (props: WarningValues): JSX.Element {
+  return (
+    <div className={styles.increasing}>
+      <img src='/imgs/assets/increase.png' alt='increasing chart'></img>
+      <span>{props.value}</span>
+    </div>
+  )
+}
