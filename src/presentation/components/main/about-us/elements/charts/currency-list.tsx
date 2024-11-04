909c7d0dd233b0dc0ab1267b38ec84566d1c0d38
Daniel <danielfalcaoviktorio@gmail.com>
2024-11-04 14:52:12 -0300

    feat: ensure currencies list render currencies with correct styles

---
+++ src/presentation/components/main/about-us/elements/charts/currency-list.tsx
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react'
+import * as styles from './currency-list-styles.scss'
+import { CurrencyDataContext } from '@/main/context/currency-data-context'
+import CurrencyIcon from '@/presentation/elements/icons/currency-icon'
+import MenuButton from '../buttons/menu-button'
+
+export default function CurrencyList (): JSX.Element {
+  const { CurrencyData } = useContext(CurrencyDataContext)
+  return (
+    <div className={styles.currencyListContainer}>
+      <div className={styles.currencyListHeader}>
+        <h1>My Portfolio</h1>
+        <MenuButton />
+      </div>
+      <div className={styles.currencyList}>
+        {CurrencyData.map((currency, index) => {
+          return (
+            <>
+              {index >= 1 ? <span></span> : ''}
+              <div key={currency.code} className={styles.currencyItem}>
+                <CurrencyIcon
+                  html={{ className: currency.code }}
+                  img={currency.code}
+                />
+                <div className={styles.currencyItemMain}>
+                  <h2>{currency.code}</h2>
+                  <p>{Math.random().toFixed(4)}</p>
+                  <h4 className={styles[Number(currency.pctChange) > 0 ? 'positive' : 'negative']}>{Number(currency.pctChange) > 0 && '+'}{currency.pctChange}%</h4>
+                  <h3>{currency.value}</h3>
+                </div>
+              </div>
+            </>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
