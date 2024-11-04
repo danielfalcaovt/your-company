d6b7d626eb8f403f736a08fe18d23ceb7b043047
Daniel <danielfalcaoviktorio@gmail.com>
2024-11-04 14:23:15 -0300

    feat: currency list menu button add

---
+++ src/presentation/components/main/about-us/elements/buttons/menu-button.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as styles from './menu-button-styles.scss'
+
+export default function MenuButton (): JSX.Element {
+  return (
+    <div className={styles.menuButton}>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
