1ba167cf8a7e335a666fe1e97b71aeb838d3957d
Daniel <danielfalcaoviktorio@gmail.com>
2024-11-12 16:34:36 -0300

    refactor: ensure LandscapeChart render values from props instead of direct on module

--- src/presentation/components/main/about-us/elements/charts/landscape-chart.tsx
+++ src/presentation/components/main/about-us/elements/charts/landscape-chart.tsx
@@ -1,18 +1,17 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import * as styles from './landscape-chart-styles.scss'
 import CurrencyIcon from '@/presentation/elements/icons/currency-icon'
 import WarningDecreasing from '../warning/warning-decreasing'
-import { CurrencyDataContext } from '@/main/context/currency-data-context'
 import WarningIncreasing from '../warning/warning-increasing'
+import { ICurrency } from '@/domain/protocols/currency'
 
-export default function LandscapeChart (): JSX.Element {
-  const { CurrencyData } = useContext(CurrencyDataContext)
+export default function LandscapeChart (props: { currency: ICurrency }): JSX.Element {
   return (
     <div className={styles.landscapeChart}>
       <div className={styles.lchartHeader}>
         <CurrencyIcon img={'BTC'} color="#e7c83c" />
         <div className={styles.lchartHeaderTitle}>
-          <h1>{CurrencyData[0].code}</h1>
+          <h1>{props.currency.code}</h1>
           <p>BRL</p>
         </div>
       </div>
@@ -20,10 +19,10 @@ export default function LandscapeChart (): JSX.Element {
         <div className={styles.chart} id='landscape-chart'></div>
       </div>
       <div className={styles.lchartFooter}>
-        <span>{CurrencyData[0].value}</span>
-        {Number(CurrencyData[0].pctChange) < 0
-          ? <WarningDecreasing value={CurrencyData[0].pctChange} />
-          : <WarningIncreasing value={CurrencyData[0].pctChange} />}
+        <span>{props.currency.value}</span>
+        {Number(props.currency.pctChange) < 0
+          ? <WarningDecreasing value={props.currency.pctChange} />
+          : <WarningIncreasing value={props.currency.pctChange} />}
       </div>
     </div>
   )
