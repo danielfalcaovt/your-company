ec7f693b8922d103751a22a8ba4b591f63e39e7d
Daniel <danielfalcaoviktorio@gmail.com>
2024-11-15 08:33:16 -0300

    feat: app icon function add from props

--- src/presentation/elements/icons/app-icon.tsx
+++ src/presentation/elements/icons/app-icon.tsx
@@ -4,11 +4,12 @@ import * as styles from './app-icon-styles.scss'
 type appIconModel = {
   imgSrc: string
   title: string
+  iconClickFunction: React.MouseEventHandler<HTMLDivElement>
 }
 
 export default function AppIcon (props: appIconModel): JSX.Element {
   return (
-    <div className={styles.appIcon}>
+    <div onClick={props.iconClickFunction} className={styles.appIcon}>
       <img src={props.imgSrc} alt={props.title + ' icon'}/>
       <h1>{props.title}</h1>
     </div>
