25686e27e1d072cfd557a4b800278e03c6039758
Daniel <danielfalcaoviktorio@gmail.com>
2024-11-21 08:58:46 -0300

    feat: ensure Main render HottestCoins

--- src/presentation/components/main/main.tsx
+++ src/presentation/components/main/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import AboutUs from './about-us/about-us'
 import Sponsors from './sponsors/sponsors'
+import HottestCoins from './hottest-coins/hottest-coins'
 
 export default function Main (): JSX.Element {
   return (
     <>
       <AboutUs />
       <Sponsors />
+      <HottestCoins/>
     </>
   )
 }
